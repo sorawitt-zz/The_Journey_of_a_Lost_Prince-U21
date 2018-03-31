9cdc3af9436bd0bffafc03dda76e62e69046b0d8
Kong <sorawitt57@email.nu.ac.th>
2018-04-01 02:27:47 +0700

    add section

---
+++ U21/Model/SectionModel.swift
@@ -0,0 +1,16 @@
+//
+//  SectionModel.swift
+//  U21
+//
+//  Created by Kong on 4/1/2561 BE.
+//  Copyright © 2561 u21. All rights reserved.
+//
+
+import Foundation
+
+struct SectionModel {
+    let tile: String
+    let img: String
+    let detail: String
+    let status: String
+}
