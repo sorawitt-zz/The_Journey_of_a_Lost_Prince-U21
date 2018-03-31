815e87d4b9779c1a374ede09c6eb816a1b97f899
Kong <sorawitt57@email.nu.ac.th>
2018-03-31 14:24:01 +0700

    - init file

---
+++ U21/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  U21
+//
+//  Created by Kong on 3/31/2561 BE.
+//  Copyright © 2561 u21. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
