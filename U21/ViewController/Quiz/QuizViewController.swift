7ee9505d920b3ef1931ee2eaeb2a6f31773d687d
Kong <sorawitt57@email.nu.ac.th>
2018-04-01 03:50:33 +0700

    - add quiz

---
+++ U21/ViewController/Quiz/QuizViewController.swift
@@ -0,0 +1,20 @@
+//
+//  QuizViewController.swift
+//  U21
+//
+//  Created by Kong on 4/1/2561 BE.
+//  Copyright © 2561 u21. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
