37dcafa86a690e60a74f39b79dd119482dc8baa6
Kong <sorawitt57@email.nu.ac.th>
2018-04-01 00:59:18 +0700

    add mini menu cell

---
+++ U21/ViewController/MiniMenu/MiniCell/MiniMenuCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MiniMenuCollectionViewCell.swift
+//  U21
+//
+//  Created by Kong on 4/1/2561 BE.
+//  Copyright © 2561 u21. All rights reserved.
+//
+
+import UIKit
+
+class MiniMenuCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
