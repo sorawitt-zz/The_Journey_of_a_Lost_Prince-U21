0e31b6e59f74bdc6b363279cf68bae1ea69f20c7
Kong <sorawitt57@email.nu.ac.th>
2018-04-01 12:43:59 +0700

    quiz

--- U21/ViewController/PinGame/PinGameViewController.swift
+++ U21/ViewController/PinGame/PinGameViewController.swift
@@ -7,29 +7,76 @@
 //
 
 import UIKit
+import Firebase
 
 class PinGameViewController: UIViewController {
 
+    
+    @IBOutlet weak var pinLabel: UILabel!
+    @IBOutlet weak var getPinBtn: UIButton!
+    @IBOutlet weak var pinTextField: UITextField!
+    @IBOutlet weak var statusText: UILabel!
+    @IBOutlet weak var startGameBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var pin = ""
+    @IBAction func getPin() {
+        let pin = generatePIN()
+        self.pin = pin
+        guard let uuidUser = Auth.auth().currentUser?.uid else { return }
+        let pinRef = Database.database().reference(withPath: "QUIZ").child(pin)
+        pinRef.setValue(["pinID": pin, "member": [uuidUser: true], "status": false])
+        pinLabel.text = "PIN: \(pin)"
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func accessPIN() {
+        guard let pinn = pinTextField.text else { return }
+        let refPIN = Database.database().reference(withPath: "QUIZ").child(pinn)
+        refPIN.observeSingleEvent(of: .value) { (snapshot) in
+            snapshot.exists()
+            if snapshot.exists() {
+                self.statusText.text = "Status : พร้อมเล่น"
+                self.nextView()
+            }else {
+                self.statusText.text = "Status : type PIN again!, please"
+            }
+        }
     }
-    */
+    
+    func nextView() {
+        Database.database().reference(withPath: "QUIZ").child(pin).child("status").observe(.value) { (snapshot) in
+                print(snapshot.value)
+            guard let statusValue = snapshot.value as? Int else { return }
+            print(statusValue)
+            if statusValue == 1 {
+                self.startGame()
+            }else {
+                
+            }
+            
+        }
+    }
+
+    func generatePIN() -> String {
+        let len = 4
+        let pswdChars = Array("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+        let rndPswd = String((0..<len).map{ _ in pswdChars[Int(arc4random_uniform(UInt32(pswdChars.count)))]})
+        return rndPswd
+    }
+    
+    @IBAction func startGame() {
+        let quizVC = QuizViewController()
+        let pinRef = Database.database().reference(withPath: "QUIZ").child(pin)
+        pinRef.updateChildValues(["status": true])
+         self.show(quizVC, sender: self)
+    }
 
 }
--- U21/ViewController/Quiz/QuizViewController.swift
+++ U21/ViewController/Quiz/QuizViewController.swift
@@ -7,14 +7,131 @@
 //
 
 import UIKit
+import Firebase
 
 class QuizViewController: UIViewController {
-
+    
+    var countdownTimer: Timer!
+    var totalTime = 20
+    var score = 0
+    var arrQuiz: [QuizModel] = []
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var soluTrueBtn: UIButton!
+    @IBOutlet weak var soluFalseBtn: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        startTimer()
+        
+        
+        arrQuiz = [
+            QuizModel(question: "อาณาจักรล้านนาตั้งอยู่ในพื้นที่ภูเขาทางภาคเหนือของประเทศไทย ", solution: true),
+            QuizModel(question: "พื้นที่ส่วนใหญ่เป็นพื้นที่ชายฝั่ง", solution: false),
+            QuizModel(question: "สภาพแวดล้อมของพื้นที่ราบในแอ่งเขามีความอุดมสมบูรณ์ ", solution: true)
+            
+        ]
+        
+//        ,
+//        QuizModel(question: "วิถีชีวิตเปลี่ยนแปลงตามสภาพแวดล้อมและพื้นที่ ", solution: true),
+//        QuizModel(question: "ชุมชนบนพื้นที่ภูเขามีการตั้งถิ่นฐานขนาดใหญ่", solution: false),
+//        QuizModel(question: "พื้นที่ภูเขาเป็นอุปสรรคในการตั้งถิ่นฐาน", solution: true),
+//        QuizModel(question: "ประกอบอาชีตเกษตรกรรมเป็นหลัก", solution: true),
+//        QuizModel(question: "ผู้คนใช้ภาษาในการสื่อสารเพียงภาษาเดียว ", solution: false),
+//        QuizModel(question: "เกิดการเรียนรู้วิถีชีวิตของผู้คนในแต่ละพื้นที่", solution: true),
+//        QuizModel(question: "การศึกษาความหลากหลายของผู้คนและพื้นที่ในอดีตทำให้เกิดการขยายโลกทัศน์ความรู้ ความเข้าใจให้แก่ผู้ศึกษามากขึ้น", solution: true)
+        
+        setQuiz(quiz: arrQuiz[0])
     }
     
+    func startTimer() {
+        countdownTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTime() {
+        timerLabel.text = "\(timeFormatted(totalTime))"
+        
+        if totalTime != 0 {
+            totalTime -= 1
+        } else {
+            endTimer()
+        }
+        
+        if totalTime % 10 == 0 && totalTime >= 2 {
+            checkAnswer()
+            nextQuiz()
+        }else if totalTime <= 1 {
+            endTimer()
+        }
+        
+    }
+    
+    var indexQuiz = 1
+    
+    func nextQuiz() {
+        indexQuiz = indexQuiz + 1
+        let quiz = arrQuiz[indexQuiz]
+        setQuiz(quiz: quiz)
+        
+        
+    }
+    var solutionsGlobal = true
+    
+    func setQuiz(quiz: QuizModel) {
+        questionLabel.text = quiz.question
+        self.solutionsGlobal = quiz.solution
+    }
+    
+    var userSolution = false
+    @IBAction func truePressed() {
+        userSolution = true
+    }
 
+    @IBAction func falsePressed() {
+        userSolution = false
+    }
+    
+    func saveScore() {
+        guard let uuids = Auth.auth().currentUser?.uid else { return }
+        
+        let rawData = Database.database().reference().child("Users").child("\(uuids)/score")
+        rawData.setValue(score)
+        
+    }
+    
+    func endTimer() {
+        countdownTimer.invalidate()
+
+        
+        saveScore()
+    
+        let scoreBoard = LeaderBoardViewController()
+        show(scoreBoard, sender: self)
+        
+    }
+    
+    func checkAnswer() {
+        if solutionsGlobal == userSolution {
+            score = score + 10
+            resultLabel.text = "คุณตอบ: ถูกต้อง"
+        }else {
+            resultLabel.text = "คุณตอบ: ผิด"
+        }
+        
+        scoreLabel.text = "Your Score: \(score)"
+        
+    }
+    
+    
+    func timeFormatted(_ totalSeconds: Int) -> String {
+        let seconds: Int = totalSeconds % 60
+        let minutes: Int = (totalSeconds / 60) % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+    
+    
+    
 }
